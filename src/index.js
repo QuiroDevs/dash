84c92a86ea3c76fefa6e076bac09c7badbb0e98c
QuiroDevs <long5656512@gmail.com>
2025-07-18 22:08:22 +0200

    Primer commit limpio

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-
-import './index.css'
+import './index.css';
 import App from './App.js';
-import { ContextProvider } from './contexts/ContextProvider.js'
+import { ContextProvider } from './contexts/ContextProvider.js';
 
 ReactDOM.render(
-    <BrowserRouter basename="/dash">
-        <ContextProvider>
-    <App />
-</ContextProvider>,
-</BrowserRouter>,
-document.getElementById('root')
+  <BrowserRouter basename="/dash">
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
